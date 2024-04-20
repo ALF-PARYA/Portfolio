ba9d88a370208b25e3501154d79eae6f4ad5fb75
parya <antoine020999@gmail.com>
2024-04-20 16:31:21 +0200

    adding essentials

--- include/header.php
+++ include/header.php
@@ -1,1 +1,18 @@
-<?php
+<div class="container-fluid d-flex justify-content-around align-items-center pt-5">
+    <h1 class="text-upercase font-text">Antoine Laforest</h1>
+
+    <div class="font-text ">
+        <ul class="d-flex gap-5">
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="../info.php">Projets</a></li>
+            <li><a href="info.php">A Propos</a></li>
+        </ul>
+    </div>
+
+    <div>
+        <a href=""><i class="bi bi-github"></i></a>
+        <a href=""><i class="bi bi-linkedin"></i></a>
+
+    </div>
+
+</div>
--- index.php
+++ index.php
@@ -1,2 +1,60 @@
 <?php
 
+include 'include/html.php';
+?>
+
+<body class="container">
+    <?php include 'include/header.php'; ?>
+
+    <main>
+
+        <!-- start -->
+        <section>
+            <div class="container-fluid d-flex justify-content-center align-items-center gap-5 ">
+                <div col-12 col-md-5>
+                    <h2 class="font-text"> Hello, je m'appelle <br> Antoine Laforest</h2>
+                    <p class="font-text">Je suis Développeur web</p>
+                    <div class="font-text">
+                        <a href=""><i class="bi bi-code-slash">Développeur</i></a>
+                        <a href=""><i class="bi bi-hand-thumbs-up">Esprit D'équipe</i></a>
+                        <a href=""><i class="bi bi-search">Curieux</i>></a>
+                    </div>
+                </div>
+                <img class="object-fit-contain  rounded col-12 col-md-5" src="assets/personnal/profile-fb.jpg" alt="">
+            </div>
+        </section>
+        <!-- arguments -->
+        <section>
+
+            <div>
+                <div>
+                    <i></i>
+                    <h2>Toujours a innové</h2>
+                    <p>je recherche constament de nouvelle facon de travailer,de nouveaux outils/frameworks/ect</p>
+                </div>
+
+                <div>
+                    <i></i>
+                    <h2>Travail d'équipe</h2>
+                    <p>j'aime le fait de travailler en équipe et d'apprendre des autres.</p>
+                </div>
+
+                <div>
+                    <i></i>
+                    <h2>Disponible</h2>
+                    <p>Je sais comment m'adapter aux situations et me tenir à disposition en cas d'urgence. </p>
+                </div>
+
+            </div>
+        </section>
+
+        <!-- last creations -->
+
+        <!-- past experiences -->
+
+
+        <?php include 'include/footer.php'; ?>
+    </main>
+</body>
+
+</html>
